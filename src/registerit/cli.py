f7b930c676847f97606f72d221a0549c3410a133
James Addison <james.addison@collabora.com>
2022-03-08 17:31:35 +0000

    Add the ability to specify an alternate PyPi repository URL

--- src/registerit/cli.py
+++ src/registerit/cli.py
@@ -2,6 +2,7 @@
 Command Line Interface (CLI) for registerit.
 """
 import click
+from twine.utils import DEFAULT_REPOSITORY
 
 from .build_and_upload import (
     build_minimal_python_distribution,
@@ -16,13 +17,15 @@ from .build_and_upload import (
 @click.option('--author', required=False, type=str, default='me')
 @click.option('--email', required=False, type=str, default='me@me.com')
 @click.option('--url', required=False, type=str, default='http://www.github.com')
+@click.option('--repository-url', required=False, type=str, default=DEFAULT_REPOSITORY)
 def cli(
     package_name: str,
     username: str,
     password: str,
     author: str,
     email: str,
-    url: str
+    url: str,
+    repository_url: str
 ) -> None:
     """Register package names on PyPI."""
     try:
@@ -32,7 +35,7 @@ def cli(
             email,
             url
         )
-        upload_distribution_to_pypi(minimal_dist, username, password)
+        upload_distribution_to_pypi(minimal_dist, username, password, repository_url)
         print(f'{package_name} successfully registered on PyPI.')
     except RuntimeError as e:
         print(f'ERROR: {e}')
